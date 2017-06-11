2547f75d3056beec2225dee4d465c01e4233c812
semiller10 <samuelmiller10@gmail.com>
2017-06-11 01:37:49 -0500

    Updated gitignore

--- separate_proteins.py
+++ separate_proteins.py
@@ -13,13 +13,16 @@ def main(fin_path, fout_path):
         with open(fout_path, 'w') as fout_handle:
             for record in SeqIO.parse(fin_handle, 'fasta'):
                 protein_seqs = record.seq._data.strip('*').split('*')
+                split_count = 0
                 for s in protein_seqs:
-                    fout_handle.write('>' + record.id + '\n')
-                    fout_handle.write(s + '\n')
+                    if len(s) > 5:
+                        fout_handle.write('>' + record.id + '_' + str(split_count) + '\n')
+                        fout_handle.write(s + '\n')
+                    split_count += 1
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = __file__.__doc__)
-    parser.add_argument('--fin_path', default = 'test.faa', help = 'Path to input faa file')
+    parser = argparse.ArgumentParser(description=__file__.__doc__)
+    parser.add_argument('--fin_path', default='test.faa', help='Path to input faa file')
     args = parser.parse_args()
     fin_path = os.path.abspath(args.fin_path)
     fout_path = os.path.join(
